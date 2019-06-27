cbd1739c0c9bb647c6604a54d57f071582a161f6
Evan Jeffries <ejeffries14@gmail.com>
2019-06-27 22:27:39 +0000

    Automatically backed up by Learn

--- cartoon_collections.rb
+++ cartoon_collections.rb
@@ -1,9 +1,13 @@
-def roll_call_dwarves# code an argument here
-  # Your code here
+def roll_call_dwarves(dwarf)
+  dwarf.each_with_index {|item, index| puts "#{index + 1}:#{item}"}
 end
 
-def summon_captain_planet# code an argument here
-  # Your code here
+def summon_captain_planet(planteer_calls)
+  elements = [] # code an argument here
+  planteer_calls.each do |element|
+    elements << "#{element.capitalize}!"
+    end
+    return elements
 end
 
 def long_planeteer_calls# code an argument here

f23c212227b6cb55d82e5f82152724fa55cc31a1
Evan Jeffries <ejeffries14@gmail.com>
2019-06-27 23:11:48 +0000

    Automatically backed up by Learn

--- cartoon_collections.rb
+++ cartoon_collections.rb
@@ -20,6 +20,6 @@ end
 def find_the_cheese(array) # code an argument here
   cheese_types = ["cheddar", "gouda", "camembert"]
   array.find do |cheese|
-    
+    cheese = "cheddar" || cheese = "gouda" || cheese = "camembert"
   end
 end

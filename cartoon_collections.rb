7319480758af0e4f6831eb6cc7266066ce106694
Evan Jeffries <ejeffries14@gmail.com>
2019-06-27 22:34:39 +0000

    Automatically backed up by Learn

--- cartoon_collections.rb
+++ cartoon_collections.rb
@@ -11,8 +11,8 @@ def summon_captain_planet(planteer_calls)
 end
 
 def long_planeteer_calls(short_words)
-  short_words.collect do 
-  
+  short_words.collect do |words|
+  if words.length 
   # Your code here
 end
 
